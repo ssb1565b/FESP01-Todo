356061abcd2770d8804b9e663e8f6bd32406676c
윤태현 <yoonth0919@gmail.com>
2023-11-06 16:09:34 +0900

    Refactor: layout 페이지 TS로 리팩토링

--- 02.typescript/todoapp/src/layout/Nav.ts
+++ 02.typescript/todoapp/src/layout/Nav.ts
@@ -1,9 +1,13 @@
-const Nav = function () {
+const Nav = function (): HTMLElement {
   const navNode = document.createElement("nav");
   const arrowBack = document.createElement("i");
   arrowBack.className = "fa-solid fa-arrow-left";
-  arrowBack.addEventListener("click", () => {
-    window.history.back();
+  arrowBack?.addEventListener("click", () => {
+    if (window.location.href.includes("info")) {
+      window.location.href = '/';
+    } else {
+      window.history.back();
+    }
   });
 
   navNode.appendChild(arrowBack);
